f51f3e4b39e3766d325aaf4d449cf3973ccf2513
mthiagoalves <m.thiago.alves@live.com>
2023-08-28 10:21:10 +0100

    Chore(app): implement custom app Status app

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getAppStatus(): string {
+    return 'Project running 🐱‍🏍🚀: \n Please check http://localhost:3454/api for Swagger Docs.';
   }
 }
