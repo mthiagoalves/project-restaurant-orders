b82a03724c07c067b7d7dcb0b5b44a6ac0fdd853
mthiagoalves <m.thiago.alves@live.com>
2023-08-29 10:41:39 +0100

    feat(product): implement dto validation

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,8 +1,51 @@
+import { ApiProperty } from "@nestjs/swagger"
+import { IsNumber, IsPositive, IsString } from "class-validator"
+
 export class CreateProductDto {
-  name: string
-  slug: string
-  description: string
-  price: number
+
+  @IsString()
+  @ApiProperty({
+    description: 'Name of the product',
+    example: 'Vegan Harumaki'
+  })
+  name: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Slug of the product generate automatic as name',
+    example: 'Vegan Harumaki'
+  })
+  slug: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Description of the product',
+    example: 'A little description about this product...'
+  })
+  description: string;
+
+  @IsNumber({
+    maxDecimalPlaces: 2
+  })
+  @IsPositive()
+  @ApiProperty({
+    description: 'Price of the product',
+    example: 5.00
+  })
+  price: number;
+
+  @IsNumber()
+  @IsPositive()
+  @ApiProperty({
+    description: 'Order for show the product',
+    example: 1
+  })
   order: number
+
+  @IsString()
+  @ApiProperty({
+    description: 'Image of the product',
+    example: 'https://i.imgur.com/hNE75Iw.png'
+  })
   image: string
 }
